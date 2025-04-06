1a8803a6efd01d1dc55a6f06f164d59e6d30aaf7
Matt Benton <matt@mattbenton.net>
2025-04-07 08:56:05 +1000

    feat: ts configs

--- src/join.ts
+++ src/join.ts
@@ -30,19 +30,13 @@ export function joinLeft<
   compare: (left: LeftRow, right: RightRow) => boolean
 ): ResultRow[] {
   const result: ResultRow[] = [];
-  // const processedLeftRows = new Set<LeftRow>();
   for (const leftRow of left) {
     let didJoin = false;
     for (const rightRow of right) {
       if (compare(leftRow, rightRow)) {
         didJoin = true;
-        // processedLeftRows.add(leftRow);
         result.push({ ...leftRow, ...rightRow } as ResultRow);
       }
-      //  else if (!processedLeftRows.has(leftRow)) {
-      //   // processedLeftRows.add(leftRow);
-      //   result.push({ ...leftRow } as unknown as ResultRow);
-      // }
     }
     if (!didJoin) {
       result.push({ ...leftRow } as unknown as ResultRow);
